ce7ea008f4fccc2aea09d8aca28211fb2c948d54
JuanPlaza-419 <juanplazahurtado@gmail.com>
2026-01-12 13:01:12 +0100

    Con funcion de evitar texto

--- katas_divisibles.py
+++ katas_divisibles.py
@@ -1,10 +1,19 @@
-numero = int(input("Ingresa un número: "))
+def fizzbuzz():
+    entrada = input("Introduce un número: ")
 
-if numero % 3 == 0 and numero % 5 == 0:
-    print("FizzBuzz")
-elif numero % 3 == 0:
-    print("Fizz")
-elif numero % 5 == 0:
-    print("Buzz")
-else:
-    print(numero)
+    if not entrada.isdigit():
+        print('No me vengas con "' + entrada + '" aquí solo hablamos con números')
+        return
+
+    numero = int(entrada)
+
+    if numero % 3 == 0 and numero % 5 == 0:
+        print("FizzBuzz")
+    elif numero % 3 == 0:
+        print("Fizz")
+    elif numero % 5 == 0:
+        print("Buzz")
+    else:
+        print("El número " + str(numero) + " no es divisible ni entre 3 ni entre 5")
+
+fizzbuzz()
